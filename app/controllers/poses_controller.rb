97f7382edb4151defacd16a9cda4921f2461a0c9
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-07-18 11:35:02 +0100

    Views added for yoga poses

--- app/controllers/poses_controller.rb
+++ app/controllers/poses_controller.rb
@@ -4,4 +4,23 @@ class PosesController < ApplicationController
 		@poses = Pose.all
 	end
 	
+	def new
+		@pose = Pose.new
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def edit 
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
 end
