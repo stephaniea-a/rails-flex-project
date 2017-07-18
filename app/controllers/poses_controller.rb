238322ce8cf260a37a4df7ce74d3d7617f288e38
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-07-18 12:01:29 +0100

    Methods created in poses controller. Create button does not appear tp be working but able to edut and update successfully

--- app/controllers/poses_controller.rb
+++ app/controllers/poses_controller.rb
@@ -9,18 +9,34 @@ class PosesController < ApplicationController
 	end
 
 	def show
+		@pose = Pose.find(params[:id])
 	end
 
 	def create
+		@pose = Pose.create(pose_params)
+		@pose.save
+		redirect_to @pose
 	end
 
 	def edit 
+		@pose = Pose.find(params[:id])
 	end
 
 	def update
+		@pose = Pose.find(params[:id])
+		@pose.update(pose_params)
+		redirect_to @pose
 	end
 
 	def destroy
+		@pose = Pose.find(params[:id])
+		@pose.destroy
+		redirect_to poses_path
 	end
+
+	private 
+		def pose_params
+			params.require(:pose).permit(:name, :sanskrit_name, :image, :description, :difficulty_level)
+		end
 
 end
